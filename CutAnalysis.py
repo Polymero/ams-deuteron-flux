d0b2762eb4d5b34c3859cd5e96876131793263aa
Nika <sebastiaanvenendaal@gmail.com>
2020-10-27 02:47:51 +0100

    update 27-10-20

--- CutAnalysis.py
+++ CutAnalysis.py
@@ -50,50 +50,64 @@ CR = ROOT.TCut('trk_rig > {0}*cf'.format(geo_crit)) # geo-magnetic cut-off
 # PLOT FUNCTION
 # ------------------------------------------------------------------------------
 
+ROOT.gStyle.SetOptTitle(0)
+
 def cut_plot(par, title='', xlabel='', ylabel='events', stats=False, bins=200, xrange=(0,0), yrange=(0,0), log_y=True, prop=False):
     # Create Canvas
     if title == '':
         title = par
-    canvas = ROOT.TCanvas("{}".format(title),"{}".format(title), 800, 500)
-    # Draw Histogram and store in ROOT.histo
+    canvas = ROOT.TCanvas("{}".format(title),"{}".format(title), 1600, 1000)
+    # Draw Histogram and store in ROOT.???
     xrange = str(xrange)[1:-1]
-    chain.Draw('{} >> hist1({}, {})'.format(par, bins, xrange), '')
-    chain.Draw('{} >> hist2({}, {})'.format(par, bins, xrange), CG, 'SAME')
-    chain.Draw('{} >> hist3({}, {})'.format(par, bins, xrange), CG+CQ, 'SAME')
-    chain.Draw('{} >> hist4({}, {})'.format(par, bins, xrange), CG+CQ+CZ, 'SAME')
-    chain.Draw('{} >> hist5({}, {})'.format(par, bins, xrange), CG+CQ+CZ+CR, 'SAME')
+    chain.Draw('{} >> {}({}, {})'.format(par, title+'1', bins, xrange), '', '')
+    chain.Draw('{} >> {}({}, {})'.format(par, title+'2', bins, xrange), CG, 'SAME')
+    chain.Draw('{} >> {}({}, {})'.format(par, title+'3', bins, xrange), CG+CQ, 'SAME')
+    chain.Draw('{} >> {}({}, {})'.format(par, title+'4', bins, xrange), CG+CQ+CZ, 'SAME')
+    chain.Draw('{} >> {}({}, {})'.format(par, title+'5', bins, xrange), CG+CQ+CZ+CR, 'SAME')
+    # Get TH1F objects
+    hist1 = ROOT.gROOT.FindObject(title+'1')
+    hist2 = ROOT.gROOT.FindObject(title+'2')
+    hist3 = ROOT.gROOT.FindObject(title+'3')
+    hist4 = ROOT.gROOT.FindObject(title+'4')
+    hist5 = ROOT.gROOT.FindObject(title+'5')
     # Coloring
-    ROOT.hist1.SetLineColor(ROOT.kBlue)
-    ROOT.hist2.SetLineColor(ROOT.kRed)
-    ROOT.hist3.SetLineColor(ROOT.kGreen)
-    ROOT.hist4.SetLineColor(ROOT.kOrange)
-    ROOT.hist5.SetLineColor(ROOT.kBlack)
-    ROOT.hist5.SetLineWidth(2)
+    hist1.SetLineColor(ROOT.kBlue); hist1.SetLineWidth(3)
+    hist2.SetLineColor(ROOT.kRed); hist2.SetLineWidth(3)
+    hist3.SetLineColor(ROOT.kGreen); hist3.SetLineWidth(3)
+    hist4.SetLineColor(ROOT.kOrange); hist4.SetLineWidth(3)
+    hist5.SetLineColor(ROOT.kBlack); hist5.SetLineWidth(5)
     # Labelling
-    ROOT.hist1.SetTitle(title)
-    ROOT.hist1.GetXaxis().SetTitle(xlabel)
-    ROOT.hist1.GetYaxis().SetTitle(ylabel)
-    ROOT.hist1.SetStats(stats)
+    # hist1.SetTitle(title) # Title is bugging, so just get rid of it (won't use it in thesis anyway)
+    hist1.GetXaxis().SetTitle(xlabel)
+    hist1.GetYaxis().SetTitle(ylabel)
+    hist1.SetStats(stats)
     # Axis
     if yrange != (0,0):
-        ROOT.hist1.SetAxisRange(yrange[0], yrange[1], "Y")
+        hist1.SetAxisRange(yrange[0], yrange[1], "Y")
     canvas.SetLogy(log_y)
     # Legend
     legend = ROOT.TLegend(0.65 ,0.6 ,0.85 ,0.75)
-    legend.AddEntry(ROOT.hist1, 'None')
-    legend.AddEntry(ROOT.hist2, 'CG')
-    legend.AddEntry(ROOT.hist3, 'CG+CQ')
-    legend.AddEntry(ROOT.hist4, 'CG+CQ+CZ')
-    legend.AddEntry(ROOT.hist5, 'CG+CQ+CZ+CR')
+    legend.AddEntry(hist1, 'None')
+    legend.AddEntry(hist2, 'CG')
+    legend.AddEntry(hist3, 'CG+CQ')
+    legend.AddEntry(hist4, 'CG+CQ+CZ')
+    legend.AddEntry(hist5, 'CG+CQ+CZ+CR')
     legend.SetLineWidth(0)
-    legend.SetTextSize(10)
+    #legend.SetTextSize(10) # This line makes text in TLegend disappear !!!
+    legend.Draw() # TLegend can be drawn here but has to returned as well !!!
     # Proportion of events
     if prop is True:
-        he1 = ROOT.hist1.GetEntries(); print('None: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
-        he1 = ROOT.hist2.GetEntries(); print('CG: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
-        he1 = ROOT.hist3.GetEntries(); print('CG+CQ: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
-        he1 = ROOT.hist4.GetEntries(); print('CG+CQ+CZ: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
-        he1 = ROOT.hist5.GetEntries(); print('CG+CQ+CZ+CR: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
+        he1 = hist1.GetEntries(); print('None: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
+        he1 = hist2.GetEntries(); print('CG: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
+        he1 = hist3.GetEntries(); print('CG+CQ: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
+        he1 = hist4.GetEntries(); print('CG+CQ+CZ: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
+        he1 = hist5.GetEntries(); print('CG+CQ+CZ+CR: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
+    # Delete histograms
+    # ROOT.hist1.SetDirectory(0)
+    # ROOT.hist2.SetDirectory(0)
+    # ROOT.hist3.SetDirectory(0)
+    # ROOT.hist4.SetDirectory(0)
+    # ROOT.hist5.SetDirectory(0)
     # Return canvas and legend elements
     return canvas, legend
 
@@ -102,31 +116,39 @@ def cut_plot(par, title='', xlabel='', ylabel='events', stats=False, bins=200, x
 # ------------------------------------------------------------------------------
 # TOF Beta (prop = True)
 c, l = cut_plot('tof_beta', title='TOF Beta', xlabel='beta (v/c)', xrange=(.01,2.), prop=True)
-c.Draw()
-l.Draw("same")
 c.Print(out_path + 'TOF_Beta.png')
 # RICH Beta
-c, l = cut_plot('rich_beta', title='RICH Beta', xlabel='beta (v/c)', xrange=(.74,1.05)); c.Draw(); l.Draw("same")
+c, l =cut_plot('rich_beta', title='RICH Beta', xlabel='beta (v/c)', xrange=(.74,1.05))
+c.Print(out_path + 'RICH_Beta.png')
 # Tracker Rigidity
-c, l = cut_plot('trk_rig', title='Tracker Rigidity', xlabel='R [GV]', xrange=(0,22), yrange=(1,1e6)); c.Draw(); l.Draw("same")
+c, l =cut_plot('trk_rig', title='Tracker Rigidity', xlabel='R [GV]', xrange=(0,22), yrange=(1,1e6))
+c.Print(out_path + 'Tracker_Rigidity.png')
 # Inner Tracker Charge
-c, l = cut_plot('trk_q_inn', title='Inner Tracker Charge', xlabel='Z [e]', xrange=(0.3,3.0), yrange=(1,1e5)); c.Draw(); l.Draw("same")
+c, l =cut_plot('trk_q_inn', title='Inner Tracker Charge', xlabel='Z [e]', xrange=(0.3,3.0), yrange=(1,1e5))
+c.Print(out_path + 'Inner_Tracker_Charge.png')
 # First Tracker Layer Charge
-c, l = cut_plot('trk_q_lay[0]', title='First Tracker Layer Charge', xlabel='Z [e]', xrange=(0.3,3.0), yrange=(1,3e4)); c.Draw(); l.Draw("same")
+c, l =cut_plot('trk_q_lay[0]', title='First Tracker Layer Charge', xlabel='Z [e]', xrange=(0.3,3.0), yrange=(1,3e4))
+c.Print(out_path + 'First_Tracker_Layer_Charge.png')
 # Bending Plane Chi-squared
-c, l = cut_plot('trk_chisqn[1]', title='Bending Plane Chi-squared', xlabel='chi-squared', xrange=(-.5,10.5), yrange=(1,3e5)); c.Draw(); l.Draw("same")
+c, l =cut_plot('trk_chisqn[1]', title='Bending Plane Chi-squared', xlabel='chi-squared', xrange=(-.5,10.5), yrange=(1,3e5))
+c.Print(out_path + 'Bending_Plane_Chisquared.png')
 # TOF Mass
-c, l = cut_plot('trk_q_inn * trk_rig * TMath::Sqrt(1/tof_beta/tof_beta - 1)', title='TOF Mass',
-                xlabel='m [GeV/c^2]', xrange=(.1,2.5), yrange=(0, 3750), log_y=False); c.Draw(); l.Draw("same")
+c, l =cut_plot('trk_q_inn * trk_rig * TMath::Sqrt(1/tof_beta/tof_beta - 1)', title='TOF Mass',
+                xlabel='m [GeV/c^2]', xrange=(.1,2.5), yrange=(0, 3750), log_y=False)
+c.Print(out_path + 'TOF_Mass.png')
 # RICH Mass
-c, l = cut_plot('trk_q_inn * trk_rig * TMath::Sqrt(1/rich_beta/rich_beta - 1)', title='RICH Mass',
-                xlabel='m [GeV/c^2]', xrange=(.1,2.5), yrange=(0, 4250), log_y=False); c.Draw(); l.Draw("same")
+c, l =cut_plot('trk_q_inn * trk_rig * TMath::Sqrt(1/rich_beta/rich_beta - 1)', title='RICH Mass',
+                xlabel='m [GeV/c^2]', xrange=(.1,2.5), yrange=(0, 4250), log_y=False)
+c.Print(out_path + 'RICH_Mass.png')
 
+# ------------------------------------------------------------------------------
+# YET TO FIX: ALL BELOW
+# ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # GEO-MAGNETIC CUT-OFF PLOTS
 # ------------------------------------------------------------------------------
 # 2D RGeo VS Rigidity
-c = ROOT.TCanvas("beta_rig","beta_rig", 800, 500)
+c, l =ROOT.TCanvas("beta_rig","beta_rig", 800, 500)
 chain.Draw('{0}*cf:trk_rig >> hist(200)'.format(geo_crit), CG+CQ+CZ, 'COLZ')
 # Labelling
 ROOT.hist.GetXaxis().SetTitle('R [GV]')
@@ -136,14 +158,13 @@ ROOT.hist.SetStats(0)
 # Axis
 c.SetLogy(False)
 c.SetLogx(False)
-# Draw
-c.Draw()
+
 
 # EFFECT OF DIFFERENT RGeo CRITERIA
 bins = 200 # number of bins
 rang = '0.01, 22' # x-axis plot range
 # Canvas and Histrograms
-c = ROOT.TCanvas("Q_lay1_inn","Q_lay1_inn", 800, 500)
+c, l =ROOT.TCanvas("Q_lay1_inn","Q_lay1_inn", 800, 500)
 chain.Draw('trk_rig >> hist1({}, {})'.format(bins, rang), CG+CQ+CZ, '')
 chain.Draw('trk_rig >> hist2({}, {})'.format(bins, rang), CG+CQ+CZ+ROOT.TCut('trk_rig>0.8*cf'), 'SAME')
 chain.Draw('trk_rig >> hist3({}, {})'.format(bins, rang), CG+CQ+CZ+ROOT.TCut('trk_rig>1.0*cf'), 'SAME')
@@ -174,8 +195,7 @@ legend.AddEntry(ROOT.hist5, '1.4 Rgeo')
 legend.SetLineWidth(0)
 legend.SetTextSize(10)
 legend.Draw("same")
-# Draw
-c.Draw()
+
 # Proportion of events
 he1 = ROOT.hist1.GetEntries(); print('\nNo Rgeo: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
 he1 = ROOT.hist2.GetEntries(); print('0.8 Rgeo: ', he1, '({:.1f} %)'.format(he1/chain_events*100))
@@ -187,7 +207,7 @@ he1 = ROOT.hist5.GetEntries(); print('1.4 Rgeo: ', he1, '({:.1f} %)'.format(he1/
 # OTHER PLOTS
 # ------------------------------------------------------------------------------
 # TOF Mass VS Rigidity
-c = ROOT.TCanvas("mass_rig","mass_rig", 800, 500)
+c, l =ROOT.TCanvas("mass_rig","mass_rig", 800, 500)
 chain.Draw('trk_q_inn * trk_rig * TMath::Sqrt(1/tof_beta/tof_beta - 1):trk_rig >> hist(200)'.format(geo_crit), CG+CQ+CZ+CR, 'COLZ')
 # Labelling
 ROOT.hist.GetXaxis().SetTitle('R [GV]')
@@ -197,11 +217,10 @@ ROOT.hist.SetStats(0)
 # Axis
 c.SetLogy(False)
 c.SetLogx(False)
-# Draw
-c.Draw()
+
 
 # RICH Mass VS Rigidity
-c = ROOT.TCanvas("rmass_rig","rmass_rig", 800, 500)
+c, l =ROOT.TCanvas("rmass_rig","rmass_rig", 800, 500)
 chain.Draw('trk_q_inn * trk_rig * TMath::Sqrt(1/rich_beta/rich_beta - 1):trk_rig >> hist(200)'.format(geo_crit), CG+CQ+CZ+CR, 'COLZ')
 # Labelling
 ROOT.hist.GetXaxis().SetTitle('R [GV]')
@@ -211,11 +230,10 @@ ROOT.hist.SetStats(0)
 # Axis
 c.SetLogy(False)
 c.SetLogx(False)
-# Draw
-c.Draw()
+
 
 # Chi-squared VS Rigidity
-c = ROOT.TCanvas("chi_rig","chi_rig", 800, 500)
+c, l =ROOT.TCanvas("chi_rig","chi_rig", 800, 500)
 chain.Draw('trk_chisqn[1]:trk_rig >> hist(200)'.format(geo_crit), CG+CQ+CZ+CR, 'COLZ')
 # Labelling
 ROOT.hist.GetXaxis().SetTitle('R [GV]')
@@ -225,11 +243,10 @@ ROOT.hist.SetStats(0)
 # Axis
 c.SetLogy(False)
 c.SetLogx(False)
-# Draw
-c.Draw()
+
 
 # Chi-squared VS TOF beta
-c = ROOT.TCanvas("chi_beta","chi_beta", 800, 500)
+c, l =ROOT.TCanvas("chi_beta","chi_beta", 800, 500)
 chain.Draw('trk_chisqn[1]:tof_beta >> hist(200)'.format(geo_crit), CG+CQ+CZ+CR, 'COLZ')
 # Labelling
 ROOT.hist.GetXaxis().SetTitle('beta (v/c)')
@@ -239,5 +256,3 @@ ROOT.hist.SetStats(0)
 # Axis
 c.SetLogy(False)
 c.SetLogx(False)
-# Draw
-c.Draw()
