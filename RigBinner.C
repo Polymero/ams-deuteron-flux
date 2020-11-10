6c851333dccd46617f8228c6f9df073f0d0d079f
Polymero <sebastiaanvenendaal@gmail.com>
2020-11-10 01:53:26 +0100

    update 09-11-20

---
+++ RigBinner.C
@@ -0,0 +1,17 @@
+// C++ macro's edited for remote ssh to Kapteyn
+// Created        23-10-20
+// Last Edited    24-10-20
+
+// Include header file(s)
+#include <iostream>
+#include <string>
+#include "Ntp.h"
+
+// Create several important
+void RigBinner() {
+
+  Float_t bin_left = (0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+  Float_t bin_right = (2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22);
+  Float_t bin_middle = (bin_left + bin_right) / 2;
+
+}
