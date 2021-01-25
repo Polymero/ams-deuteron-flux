6f6fe3f97941f8b8adbb7a7d46f51111a39f613d
Polymero <sebastiaanvenendaal@gmail.com>
2021-01-25 01:17:34 +0100

    update 24-01-21

--- FullAnalysis.C
+++ FullAnalysis.C
@@ -1133,7 +1133,7 @@ void Anaaqra::AerogelSlice(){
 
   // Create canvasses
   TCanvas *ABMcanvas = new TCanvas("ABMcanvas", "Aerogel Beta Mass");
-  aero_beta_mass->Draw();
+  aero_beta_mass->Draw("COLZ");
   ABMcanvas->Draw(); ABMcanvas->Print("./ProtonAnalysis/Aerogel Beta Mass.png");
 
 }
